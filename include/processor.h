fd1f01603c228bb16552ecaf8dc3b20acf77a949
hyper-make <67819904+hyper-make@users.noreply.github.com>
2020-08-31 21:45:59 -0400

    Update processor.h

--- include/processor.h
+++ include/processor.h
@@ -7,6 +7,17 @@ class Processor {
 
   // TODO: Declare any necessary private members
  private:
+  int previdle = 0;
+  int previowait = 0;
+  int prevuser = 0;
+  int prevnice = 0;
+  int prevsystem = 0;
+  int previrq = 0;
+  int prevsoftirq = 0;
+  int prevsteal = 0;
+  
+  const std::string kProcDirectory{"/proc/"};
+  const std::string kStatFilename{"/stat"};
 };
 
 #endif
